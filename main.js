4a5bbff17b7dfcd6ae4f9482075513d85dfd52d4
David Carranco <david_carranco1111@outlook.es>
2025-10-15 12:00:14 -0600

    Se agrega la función mutiplica en el main.js y se actualiza el README.md

--- main.js
+++ main.js
@@ -8,5 +8,11 @@ function resta(num1, num2) {
       return resultado;
 }// resta
 
+function multiplica(num1, num2) {
+      let resultado = num1 * num2;
+      return resultado;
+}// multiplica
+
+
 
 
